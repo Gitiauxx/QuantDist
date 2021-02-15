cc1c85a7f7a1085061fdc7b959ff225fb8c95a39
gitiauxx <xavier.gitiaux@gmail.com>
2021-02-15 09:33:58 -0500

    Test 8 circuit

--- source/utils.py
+++ source/utils.py
@@ -1,7 +1,31 @@
+import logging
+import sys
+
 import numpy as np
 
 ZERO = np.array([1, 0])
 ONE = np.array([0, 1])
+
+def get_logger(name):
+    """
+    Return a logger for current module
+    Returns
+    -------
+
+    logger : logger instance
+
+    """
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter(fmt="%(asctime)s %(levelname)s %(name)s: %(message)s",
+                                  datefmt="%Y-%m-%d - %H:%M:%S")
+    console = logging.StreamHandler(sys.stdout)
+    console.setLevel(logging.DEBUG)
+    console.setFormatter(formatter)
+
+    logger.addHandler(console)
+
+    return logger
 
 def apply_one_qbit_gate(x, n, gate):
     """
