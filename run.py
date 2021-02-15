422ef77cc8b461d3f6a4b149c358fa6e96cec9b0
gitiauxx <xavier.gitiaux@gmail.com>
2021-02-15 09:35:58 -0500

    Test 8 circuit

--- run.py
+++ run.py
@@ -1,17 +1,20 @@
 from source.utils import *
 from source.swap_test import swap_test_8Q
 
+logger = get_logger(__name__)
 
-for i in range(8):
-    for j in range(i):
-        print(j)
-        phi_list = [ZERO] * (j - 1)
-        phi_list.append(ONE)
-        phi_list = phi_list + [ZERO] * (i - j -1)
-        phi_list.append(ONE)
-        phi_list = phi_list + [ZERO] * (8 - i - 1)
+if __name__ == '__main__':
 
-        phi = np.row_stack(phi_list)
+    for i in range(8):
+        for j in range(i):
+            logger.info(j)
+            phi_list = [ZERO] * (j - 1)
+            phi_list.append(ONE)
+            phi_list = phi_list + [ZERO] * (i - j -1)
+            phi_list.append(ONE)
+            phi_list = phi_list + [ZERO] * (8 - i - 1)
 
-        meas = swap_test_8Q(phi)
-        print(meas)
+            phi = np.row_stack(phi_list)
+
+            meas = swap_test_8Q(phi)
+            logger.info(f'For pair {i, j}, measurements: {meas}')
