b1df2745be6a3be745163c73e2a26b0fce6cc54b
gitiauxx <xavier.gitiaux@gmail.com>
2021-02-15 09:30:35 -0500

    Test 8 circuit

--- run.py
+++ run.py
@@ -2,17 +2,16 @@ from source.utils import *
 from source.swap_test import swap_test_8Q
 
 
-if __name__ == '__main__':
+for i in range(8):
+    for j in range(i):
+        print(j)
+        phi_list = [ZERO] * (j - 1)
+        phi_list.append(ONE)
+        phi_list = phi_list + [ZERO] * (i - j -1)
+        phi_list.append(ONE)
+        phi_list = phi_list + [ZERO] * (8 - i - 1)
 
-    for i in range(8):
-        for j in range(i):
-            phi_list = [ZERO] * (j - 1)
-            phi_list.append(ONE)
-            phi_list = phi_list + [ZERO] * (i - j -1)
-            phi_list.append(ONE)
-            phi_list = phi_list + [ZERO] * (8 - i - 1)
+        phi = np.row_stack(phi_list)
 
-            phi = np.row_stack(phi_list)
-
-            meas = swap_test_8Q(phi)
-            print(meas)
+        meas = swap_test_8Q(phi)
+        print(meas)
